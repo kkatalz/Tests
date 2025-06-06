cedea5ae135a86e550f5cddb340bcf03929f5fbe
kkatalz <zlata.kiev2004@gmail.com>
2025-06-06 18:28:17 +0300

    Implement tests for FizzBuzz algorithm.

---
+++ tests/exercise1.test.js
@@ -0,0 +1,32 @@
+const lib = require("../exercise1");
+
+describe("fizzBuzz", () => {
+  it("should throw an exception if input is not a number", () => {
+    const args = [null, undefined, NaN, "", 0, false, {}, []];
+    args.forEach((arg) => {
+      expect(() => {
+        lib.fizzBuzz(a);
+      }).toThrow();
+    });
+  });
+
+  it("should return FizzBuzz if number is divisble by 3 and 5", () => {
+    const result = lib.fizzBuzz(15);
+    expect(result).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if number is divisble by 3 and not 5", () => {
+    const result = lib.fizzBuzz(6);
+    expect(result).toBe("Fizz");
+  });
+
+  it("should return Buzz if number is divisble by 5 and not 3", () => {
+    const result = lib.fizzBuzz(10);
+    expect(result).toBe("Buzz");
+  });
+
+  it("should return number if it's not divisble either by 5 or 3", () => {
+    const result = lib.fizzBuzz(11);
+    expect(result).toBe(11);
+  });
+});
