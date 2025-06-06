b768e8c24c4bd2ba0598d52e41d1ad56070e8bf9
kkatalz <zlata.kiev2004@gmail.com>
2025-06-06 14:57:41 +0300

    Test strings.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -16,3 +16,10 @@ describe("absolute", () => {
     expect(result).toBe(0);
   });
 });
+
+describe("greet", () => {
+  it("should return the greeting messsage", () => {
+    const result = lib.greet("Mosh");
+    expect(result).toMatch(/Mosh/);
+  });
+});
